67d3dd2f974625ab2a76bf909951ca4e132cef03
viile <admin@AdmindeMac-mini.local>
2023-01-07 12:00:59 +0800

    init

--- main.go
+++ main.go
@@ -1,15 +1,80 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
-	"time"
+	"strings"
 )
 
+var startStr = `package %s
+
+import (
+	"context"
+	"github.com/go-kit/kit/endpoint"
+)
+`
+var endpointStr = `func make%sEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(*%sRequest)
+		res, err := s.%s(ctx, req)
+		return res, err
+	}
+}`
+
 func main() {
-	fmt.Println(time.Now().Unix())
-	fmt.Println("命令行参数数量:", len(os.Args))
-	for k, v := range os.Args {
-		fmt.Printf("args[%v]=[%v]\n", k, v)
+	if len(os.Args) != 3 {
+		return
+	}
+
+	pkNames := strings.Split(os.Args[1], "/")
+	if len(pkNames) <= 0 {
+		return
+	}
+	pkName := pkNames[len(pkNames)-1]
+
+	rf, err := os.Open(os.Args[1] + "/" + os.Args[2])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer rf.Close()
+
+	wf, err := os.Open(os.Args[1] + "/endpoint.go")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer wf.Close()
+
+	w := bufio.NewWriter(wf)
+	defer w.Flush()
+
+	var begin bool
+	br := bufio.NewReader(rf)
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		if !begin {
+			if strings.Contains(string(a), "interface") {
+				begin = true
+			}
+			w.WriteString(fmt.Sprintf(startStr, pkName))
+		} else {
+			// 结束
+			if strings.Contains(string(a), "}") {
+				return
+			}
+			ts := strings.Split(strings.TrimLeft(string(a), " "), "(")
+			if len(ts) <= 0 {
+				continue
+			}
+			w.WriteString(fmt.Sprintf(endpointStr, ts[0]))
+		}
+
 	}
 }
