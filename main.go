22c7c76d96dc290154f39206a8a050b762357f4f
viile <admin@AdmindeMac-mini.local>
2023-01-07 11:45:42 +0800

    first commit

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	fmt.Println(time.Now().Unix())
+	fmt.Println("命令行参数数量:", len(os.Args))
+	for k, v := range os.Args {
+		fmt.Printf("args[%v]=[%v]\n", k, v)
+	}
 }
